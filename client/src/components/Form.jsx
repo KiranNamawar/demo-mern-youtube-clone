92df2010699ff1305bd05ff3402f815b2d61a1e9
Kiran Namawar <kirannamawar@gmail.com>
2025-12-28 19:26:31 +0530

    feat(client/forms): Implement generic form handling
    Introduces a reusable form system for the client application.
    
    Form Component:
    - Adds `Form.jsx` for creating dynamic forms with client-side validation using Zod and server interaction.
    - Manages form state, handles input changes, and processes both client and server-side validation errors.
    - Supports configurable fields, schema, submission path, and success/error callbacks.
    
    FormField Component:
    - Adds `FormField.jsx` for rendering individual form inputs.
    - Includes a password visibility toggle for "password" type fields.
    - Displays validation messages associated with each field.
    
    API Utility:
    - Adds `api.js` to create and configure an Axios instance for making consistent HTTP requests to the backend API.
    
    Error Codes:
    - Adds `error-codes.js` to define standardized error codes for consistent error handling across the client.
    
    Dependencies:
    - Installs `axios` for HTTP client operations.
    - Installs `zod` for robust schema validation.

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,77 @@
+import z from "zod";
+import { useState, useMemo } from "react";
+import api from "../lib/api";
+import FormField from "./FormField";
+import ErrorCodes from "../lib/error-codes";
+
+function Form({ fields, schema, submitPath, onSuccess, onError }) {
+  const defaultFormValues = useMemo(
+    () => Object.fromEntries(fields.map((f) => [f.name, f.defaultValue])),
+    [fields]
+  );
+  const [formValues, setFormValues] = useState(defaultFormValues);
+  const [formErrors, setFormErrors] = useState({});
+
+  function handleChange(name, value) {
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+
+    // clear field errors on change
+    if (formErrors[name]) {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: undefined,
+      }));
+    }
+  }
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    // client-side validation
+    const result = schema.safeParse(formValues);
+    if (!result.success) {
+      const error = z.treeifyError(result.error);
+      setFormErrors(error.properties ?? {});
+      return;
+    }
+
+    // send to server
+    try {
+      const response = await api.post(submitPath, result.data);
+      setFormErrors({});
+      onSuccess(response.data.data);
+    } catch (err) {
+      if (err.response) {
+        const { code, error } = err.response.data;
+
+        // handle server side validation errors
+        if (code === ErrorCodes.VALIDATION_ERROR) {
+          setFormErrors(err.response.data.errors);
+          return;
+        }
+
+        onError(code, error);
+      } else {
+        onError(ErrorCodes.NETWORK_ERROR, err.message);
+      }
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {fields.map(({ name, type }) => (
+        <FormField
+          key={name}
+          name={name}
+          type={type}
+          value={formValues[name]}
+          onChange={handleChange}
+          errors={formErrors[name]?.errors}
+        />
+      ))}
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Form;
---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Eye, EyeClosed } from "lucide-react";
+
+function FormField({ name, type, value, onChange, errors }) {
+  const [showPassword, setShowPassword] = useState(false);
+  const inputType = type === "password" && showPassword ? "text" : type;
+  return (
+    <div key={name}>
+      <label htmlFor={name}>{name}</label>
+      <div>
+        <input
+          id={name}
+          name={name}
+          type={inputType}
+          value={value}
+          onChange={(evt) => onChange(name, evt.target.value)}
+          className="border"
+        />
+        {type === "password" && (
+          <button type="button" onClick={() => setShowPassword((v) => !v)}>
+            {showPassword ? <EyeClosed /> : <Eye />}
+          </button>
+        )}
+      </div>
+      {errors && (
+        <div>
+          {errors.map((err) => (
+            <p key={err}>{err}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FormField;
